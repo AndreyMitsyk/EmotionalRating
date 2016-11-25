443e0dd03b805485d91d2a18b90346cc63e6877a
Andrey Mitsyk <mitzyk@mail.ru>
2016-11-25 15:47:35 +0300

    Added signalR to the project.

---
+++ EmotionalRatingBot/Storage/TableStorageProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmotionalRatingBot.Storage
+{
+    public class TableStorageProvider
+    {
+    }
+}
