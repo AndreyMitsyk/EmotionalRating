c42686109daf328c70196b7bfd90e7e2037836ea
Pavel Shabalin <v-pashab@microsoft.com>
2016-11-25 13:50:21 +0300

    Adds base image processing interface.

---
+++ EmotionalRatingBot/ImageCore/ImageProcessingService.cs
@@ -0,0 +1,37 @@
+﻿namespace EmotionalRatingBot.ImageCore
+{
+    using System;
+
+    using EmotionalRatingBot.Common;
+
+    public class ImageProcessingService
+    {
+        #region Init
+        private static ImageProcessingService service;
+        private static Object mux = new Object();
+
+        public static ImageProcessingService getInstance(string name)
+        {
+            if (service == null)
+            {
+                lock (mux)
+                {
+                    if (service == null)
+                        service = new ImageProcessingService();
+                }
+            }
+            return service;
+        }
+
+        private ImageProcessingService()
+        {
+            
+        }
+        #endregion
+
+        public void Process(IProcessingOrder processingOrder)
+        {
+            
+        }
+    }
+}
---
+++ EmotionalRatingBot/ImageCore/LiteImageMask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmotionalRatingBot.ImageCore
+{
+    using System.Drawing;
+    using System.Drawing.Imaging;
+
+    class LiteImageMask
+    {
+        private Bitmap mask;
+        public LiteImageMask(Image sourceImage)
+        {
+            this.mask = new Bitmap(sourceImage.Width, sourceImage.Height, PixelFormat.Format32bppArgb);
+            using (var g = Graphics.FromImage(this.mask))
+            {
+                g.Clear(Color.Transparent);
+            }
+        }
+
+    }
+}
